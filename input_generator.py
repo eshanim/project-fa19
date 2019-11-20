87f7f87ddc2f8b3dcf1d6c44ccbeaf49d03417f2
Melissa Milligan <melissa.milligan@berkeley.edu>
2019-11-20 14:03:25 -0800

    matrix

--- input_generator.py
+++ input_generator.py
@@ -3,3 +3,13 @@ num_of_locations = random.randrange(30, 50)
 num_of_tas = random.randrange(10, 25)
 
 adjacency = [["x"] * num_of_locations] * num_of_locations
+
+for i in range(length(adjacency)):
+	for j in length(i):
+		if j != i:
+			randy = round(random.uniform(.1, 20), 5)
+			adjacency[i][j] = randy
+			adjacency[j][i] = randy
+
+
+
