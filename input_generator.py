5cfcdf44c0cea86e142c30494950026c375b5d37
Eshani <eshanimehta@berkeley.edu>
2019-11-20 14:55:41 -0800

    a little buggy

--- input_generator.py
+++ input_generator.py
@@ -1,9 +1,24 @@
 import random
 from utils import *
-num_of_locations = 5 #random.randrange(30, 50)
-num_of_tas = 3 #random.randrange(10, 25)
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+import os
+import argparse
+import utils
+import networkx as nx
+import numpy as np
+from student_utils import *
+from input_validator import *
 
-adjacency = [["x"] * num_of_locations] * num_of_locations
+num_of_locations = random.randrange(30, 50)
+num_of_tas = random.randrange(10, 25)
+
+def validate_input(input_file, params=[]):
+    message, error = tests(input_file, params)
+    return error
+
+adjacency = [["x" for i in range(num_of_locations)] for j in range(num_of_locations)]
 
 for i in range(len(adjacency)):
 	for j in range(len(adjacency)):
@@ -37,3 +52,22 @@ write_to_file(f, "\n", True)
 write_to_file(f, str_loc.join(homes), True)
 write_to_file(f, "\n", True)
 write_data_to_file(f, adjacency, "\n", True)
+
+for i in range(random.randint(num_of_locations, 5*num_of_locations)):
+	randi = random.randint(0, num_of_locations - 1)
+	randj = random.randint(0, num_of_locations - 1)
+	stored = adjacency[randi][randj]
+	adjacency[randi][randj] = "x"
+	adjacency[randj][randi] = "x"
+	write_to_file(f, str(num_of_locations))
+	write_to_file(f, "\n", True)
+	write_to_file(f, str(num_of_tas), True)
+	write_to_file(f, "\n", True)
+	write_to_file(f, str_loc.join(locs), True)
+	write_to_file(f, "\n", True)
+	write_to_file(f, str_loc.join(homes), True)
+	write_to_file(f, "\n", True)
+	write_data_to_file(f, adjacency, "\n", True)
+	if validate_input(f):
+		adjacency[randi][randj] = stored
+		adjacency[randj][randi] = stored
