c0b02b877003769af2d29fe814134133bdcdc224
Wailea <waileasiler@yahoo.com>
2019-11-20 16:46:45 -0800

    added triangle inequality check

--- input_generator.py
+++ input_generator.py
@@ -16,31 +16,47 @@ num_of_tas = random.randrange(10, 25)
 
 def validate_input(input_file, params=[]):
     message, error = tests(input_file, params)
+    print(message)
     return error
 
 adjacency = [["x" for i in range(num_of_locations)] for j in range(num_of_locations)]
 
 for i in range(len(adjacency)):
-	for j in range(len(adjacency)):
-		if j != i:
-			randy = round(random.uniform(.1, 20), 5)
-			adjacency[i][j] = randy
-			adjacency[j][i] = randy
+    for j in range(len(adjacency)):
+        if j != i:
+            randy = round(random.uniform(.1, 20), 5)
+            adjacency[i][j] = randy
+            adjacency[j][i] = randy
 
 locs = []
 for i in range(num_of_locations):
-	locs.append("L" + str(i) + " ")
+    locs.append("L" + str(i) + " ")
+print(locs)
+start_point = random.choice(locs)
 
 homes = []
 count = num_of_tas
 while (count > 0):
-	home = random.choice(locs)
-	if home not in homes:
-		homes.append(home)
-		count -= 1
+    home = random.choice(locs)
+    if home not in homes:
+        homes.append(home)
+        count -= 1
 
 str_loc = ""
 str_home = ""
+
+
+G, m= adjacency_matrix_to_graph(adjacency)
+
+
+shortest = dict(nx.floyd_warshall(G))
+for u, v, datadict in G.edges(data=True):
+    if abs(shortest[u][v] - datadict['weight']) >= 0.00001:
+        i = u
+        j = v
+        adjacency[i][j] = round(shortest[u][v], 5)
+        adjacency[j][i] = round(shortest[u][v], 5)
+
 
 f = "50.in"
 write_to_file(f, str(num_of_locations))
@@ -51,23 +67,34 @@ write_to_file(f, str_loc.join(locs), True)
 write_to_file(f, "\n", True)
 write_to_file(f, str_loc.join(homes), True)
 write_to_file(f, "\n", True)
-write_data_to_file(f, adjacency, "\n", True)
+write_to_file(f, start_point, True)
+write_to_file(f, "\n", True)
+for list in adjacency:
+    write_data_to_file(f, list, " ", True)
+    write_to_file(f, "\n", True)
+
+
 
 for i in range(random.randint(num_of_locations, 5*num_of_locations)):
-	randi = random.randint(0, num_of_locations - 1)
-	randj = random.randint(0, num_of_locations - 1)
-	stored = adjacency[randi][randj]
-	adjacency[randi][randj] = "x"
-	adjacency[randj][randi] = "x"
-	write_to_file(f, str(num_of_locations))
-	write_to_file(f, "\n", True)
-	write_to_file(f, str(num_of_tas), True)
-	write_to_file(f, "\n", True)
-	write_to_file(f, str_loc.join(locs), True)
-	write_to_file(f, "\n", True)
-	write_to_file(f, str_loc.join(homes), True)
-	write_to_file(f, "\n", True)
-	write_data_to_file(f, adjacency, "\n", True)
-	if validate_input(f):
-		adjacency[randi][randj] = stored
-		adjacency[randj][randi] = stored
+    randi = random.randint(0, num_of_locations - 1)
+    randj = random.randint(0, num_of_locations - 1)
+    stored = adjacency[randi][randj]
+    adjacency[randi][randj] = "x"
+    adjacency[randj][randi] = "x"
+    write_to_file(f, str(num_of_locations))
+    write_to_file(f, "\n", True)
+    write_to_file(f, str(num_of_tas), True)
+    write_to_file(f, "\n", True)
+    write_to_file(f, str_loc.join(locs), True)
+    write_to_file(f, "\n", True)
+    write_to_file(f, str_loc.join(homes), True)
+    write_to_file(f, "\n", True)
+    write_to_file(f, start_point, True)
+    write_to_file(f, "\n", True)
+    for l in adjacency:
+        write_data_to_file(f, l, " ", True)
+        write_to_file(f, "\n", True)
+    print(validate_input(f))
+    if validate_input(f):
+        adjacency[randi][randj] = stored
+        adjacency[randj][randi] = stored
