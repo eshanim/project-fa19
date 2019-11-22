2aec64c330a6778177bbd668bae18a68309d2c27
Wailea <waileasiler@yahoo.com>
2019-11-21 18:49:00 -0800

    added simple solver

--- solver.py
+++ solver.py
@@ -25,6 +25,11 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
+    path = [starting_car_location]
+    dict = {}
+    dict[0] = list_of_homes
+    return path, dict
+
     pass
 
 """
